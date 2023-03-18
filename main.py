d86e83bbaaed3592d0131e7d585982cec42044e8
K1SKEE <wenzelx.valve@gmail.com>
2023-03-18 03:13:57 +0200

    added user_router to main_api_router

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.routing import APIRouter
 
+from api.handlers import user_router
+
 app = FastAPI(title="vocabulary-app")
 
-main_api_router = APIRouter()
+main_api_router = APIRouter(prefix="/api/v1")
 
+main_api_router.include_router(user_router, prefix='/user', tags=["user"])
 app.include_router(main_api_router)
 
 if __name__ == '__main__':
