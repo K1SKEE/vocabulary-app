4df0bf91237566c359637650b7a93a7cdf3b6c93
K1SKEE <wenzelx.valve@gmail.com>
2023-03-20 23:18:13 +0200

    Added _get_user_for_auth and _authenticate_user async functions for authentication.

--- api/services.py
+++ api/services.py
@@ -1,19 +1,47 @@
+from fastapi.security import OAuth2PasswordBearer
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.schemas import UserCreate
+from api.schemas import UserCreateForm, UserCreateResponse, Token
 from db.managers import UserManager
-from api.utils import hash_password
+from db.models import User
+from api.utils import Hasher, create_access_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
 
 
-async def _create_new_user(body: UserCreate,
-                           session: AsyncSession) -> UserCreate:
+async def _create_new_user(body: UserCreateForm,
+                           session: AsyncSession) -> UserCreateResponse | None:
+    if body.password_1 != body.password_2:
+        return
+    hashed_password, salt = Hasher.hash_password(body.password_1)
     async with session.begin():
         user_manager = UserManager(session)
-        hashed_password, salt = hash_password(body.password)
         await user_manager.create_user(
             username=body.username,
             hashed_password=hashed_password,
             salt=salt
         )
-        return UserCreate(username=body.username,
-                          password=body.password)
+        return UserCreateResponse(username=body.username,
+                                  password=body.password_1)
+
+
+async def _get_user_for_auth(
+        username: str,
+        session: AsyncSession) -> User | None:
+    async with session.begin():
+        user_manager = UserManager(session)
+        return await user_manager.get_user(username=username)
+
+
+async def _authenticate_user(username: str,
+                             password: str,
+                             session: AsyncSession
+                             ) -> Token | None:
+    user = await _get_user_for_auth(username, session)
+    if user is None:
+        return
+    if not Hasher.check_password(password, user.hashed_password, user.salt):
+        return
+    token = create_access_token(user=user)
+    return token
