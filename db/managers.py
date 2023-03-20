6973dc454f4930fce3ec43a8096d00b24a2cea15
K1SKEE <wenzelx.valve@gmail.com>
2023-03-20 22:58:41 +0200

    added get_user method to UserManager

--- db/managers.py
+++ db/managers.py
@@ -1,6 +1,7 @@
+from sqlalchemy import select, update, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from db.models import User
+from db.models import User, Dictionary
 
 
 class UserManager:
@@ -9,7 +10,7 @@ class UserManager:
         self.db_session = db_session
 
     async def create_user(self, username: str, hashed_password: str,
-                          salt: str) -> User:
+                          salt: bytes) -> User:
         new_user = User(
             username=username,
             hashed_password=hashed_password,
@@ -18,3 +19,10 @@ class UserManager:
         self.db_session.add(new_user)
         await self.db_session.flush()
         return new_user
+
+    async def get_user(self, username: str) -> User | None:
+        query = select(User).where(User.username == username)
+        result = await self.db_session.execute(query)
+        user_data = result.fetchone()
+        if user_data is not None:
+            return user_data[0]
