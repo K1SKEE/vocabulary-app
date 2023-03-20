dfb6e7d8a7572bbdd750bff5f3dc24551dfde8ba
K1SKEE <wenzelx.valve@gmail.com>
2023-03-20 23:04:26 +0200

    Added SECRET_KEY, ALGORITHM, and ACCESS_TOKEN_EXPIRE_MINUTES variables

--- settings.py
+++ settings.py
@@ -8,3 +8,10 @@ REAL_DATABASE_URL = os.getenv(
     'DATABASE_URL',
     default='postgresql+asyncpg://postgres:postgres@0.0.0.0:5432/postgres'
 )
+
+SECRET_KEY: str = os.getenv('SECRET_KEY', default='secret_key')
+
+ALGORITHM: str = os.getenv('ALGORITHM', default='HS256')
+
+ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES',
+                                                 default='10'))
