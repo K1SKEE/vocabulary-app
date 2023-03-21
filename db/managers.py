d612eedb6fd93f8fd0225faa5a7a5a7091d17461
K1SKEE <wenzelx.valve@gmail.com>
2023-03-21 13:41:57 +0200

    added DictionaryManager and add_to_vocabulary method to add new words to vocabulary

--- db/managers.py
+++ db/managers.py
@@ -26,3 +26,22 @@ class UserManager:
         user_data = result.fetchone()
         if user_data is not None:
             return user_data[0]
+
+
+class DictionaryManager:
+
+    def __init__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def add_to_vocabulary(
+            self, eng: str,
+            ukr: str,
+            user: User) -> Dictionary:
+        new_word = Dictionary(
+            eng=eng,
+            ukr=ukr,
+            user_id=user.user_id
+        )
+        self.db_session.add(new_word)
+        await self.db_session.flush()
+        return new_word
