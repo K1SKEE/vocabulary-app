bcdab27eaddd831ae907c2ae05dc647776fb1917
K1SKEE <wenzelx.valve@gmail.com>
2023-03-24 14:25:59 +0200

    Included login router in main API router with prefix '/login' and tag 'login' and added reload param for app

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.routing import APIRouter
 
-from api.handlers import user_router
+from api.handlers import user_router, login_router
 
 app = FastAPI(title="vocabulary-app")
 
 main_api_router = APIRouter(prefix="/api/v1")
 
 main_api_router.include_router(user_router, prefix='/user', tags=["user"])
+main_api_router.include_router(login_router, prefix="/login", tags=["login"])
 app.include_router(main_api_router)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
