0f6af9f7e9610b48713bd75ab849c4da119e50cd
K1SKEE <wenzelx.valve@gmail.com>
2023-03-29 01:42:40 +0300

    "Changed message sending format to JSON in send_personal_message method"

--- api/utils.py
+++ api/utils.py
@@ -64,8 +64,8 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    async def send_personal_message(self, message: str, websocket: WebSocket):
-        await websocket.send_text(message)
+    async def send_personal_message(self, message: dict, websocket: WebSocket):
+        await websocket.send_json(message)
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
