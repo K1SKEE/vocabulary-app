b22e9671db8f20cb8f4dc5fcc173994c9b801956
K1SKEE <wenzelx.valve@gmail.com>
2023-03-30 01:45:51 +0300

    minor changes

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.routing import APIRouter
+from fastapi.middleware.cors import CORSMiddleware
 
-from api.handlers import user_router
+from api.handlers import user_router, login_router
 
 app = FastAPI(title="vocabulary-app")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 main_api_router = APIRouter(prefix="/api/v1")
 
 main_api_router.include_router(user_router, prefix='/user', tags=["user"])
+main_api_router.include_router(login_router, prefix="/token", tags=["token"])
 app.include_router(main_api_router)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
